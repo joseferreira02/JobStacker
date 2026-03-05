430107f559b286327f1896bf248459138faaed1b
joseferreira02 <up202108836@g.uporto.pt>
2026-03-05 17:15:22 +0000

    Deploying Stack

---
+++ backend/config/config.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  },
+  /*
+  test: {
+    username: process.env.DB_USER_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
+    dialect: process.env.DB_DIALECT_TEST
+  },
+  production: {
+    username: process.env.DB_USER_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.DB_HOST_PROD,
+    dialect: process.env.DB_DIALECT_PROD
+  }
+  */
+};
+
---
+++ backend/src/app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { Sequelize } = require('sequelize');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const sequelize = new Sequelize(
+    process.env.DEV_DB_NAME,
+    process.env.DEV_DB_USER,
+    process.env.DEV_DB_PASS,
+    {
+        host: process.env.DEV_DB_HOST,
+        port: process.env.DEV_DB_PORT,
+        dialect: 'postgres',
+    }
+);
+
+// test connection
+app.get('/', async (req, res) => {
+    try {
+        await sequelize.authenticate();
+        res.send('Database connected');
+    } catch (err) {
+        console.error("Cant connect", err);
+        res.status(500).send("DB connection failed");
+    }
+});
+
+app.listen(3001, () => {
+    console.log("Server Running on port 3001");
+});
+
